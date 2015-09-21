6f6cd78f28112b050f9206042b08de4e7f5d4eec
William Ma <williampma@users.noreply.github.com>
2015-09-21 17:05:57 +0800

    Initial addition of VariableNode custom class

---
+++ opencog/atoms/core/VariableNode.h
@@ -0,0 +1,66 @@
+/*
+ * opencog/atoms/core/VariableNode.h
+ *
+ * Copyright (C) 2015 OpenCog Foundation
+ * All Rights Reserved
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License v3 as
+ * published by the Free Software Foundation and including the exceptions
+ * at http://opencog.org/wiki/Licenses
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program; if not, write to:
+ * Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#ifndef _OPENCOG_VARIABLE_NODE_H
+#define _OPENCOG_VARIABLE_NODE_H
+
+#include <opencog/atomspace/Handle.h>
+#include <opencog/atomspace/Node.h>
+
+namespace opencog
+{
+/** \addtogroup grp_atomspace
+ *  @{
+ */
+
+class VariableNode : public Node
+{
+protected:
+
+	VariableNode(Type,  const std::string& s,
+	           TruthValuePtr tv = TruthValue::DEFAULT_TV(),
+	           AttentionValuePtr av = AttentionValue::DEFAULT_AV());
+
+	void init(void);
+
+public:
+
+	VariableNode(const std::string& s,
+	           TruthValuePtr tv = TruthValue::DEFAULT_TV(),
+	           AttentionValuePtr av = AttentionValue::DEFAULT_AV());
+
+	VariableNode(Node&);
+};
+
+typedef std::shared_ptr<VariableNode> VariableNodePtr;
+static inline VariableNodePtr VariableNodeCast(const Handle& h)
+	{ AtomPtr a(h); return std::dynamic_pointer_cast<VariableNode>(a); }
+static inline VariableNodePtr VariableNodeCast(AtomPtr a)
+	{ return std::dynamic_pointer_cast<VariableNode>(a); }
+
+// XXX temporary hack ...
+#define createVariableNode std::make_shared<VariableNode>
+
+/** @}*/
+}
+
+#endif // _OPENCOG_VARIABLE_NODE_H
